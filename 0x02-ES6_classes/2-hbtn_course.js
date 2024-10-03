23f29b8411d620a350ee02e2957e32fc1ee1035f
beckyshaz <sharonsakah8@gmail.com>
2024-10-03 13:43:02 +0000

    thown an error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,14 +2,20 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') {
       this._name = name;
+    } else {
+      throw new Error('Name must be a string');
     }
     if (typeof length === 'number') {
       this._length = length;
+    } else {
+      throw new Error('Length must be a number');
     }
     if (
       Array.isArray(students)
       && students.every((student) => typeof student === 'string')) {
       this._students = students;
+    } else {
+      throw new Error('Students must be an array of strings');
     }
   }
 
