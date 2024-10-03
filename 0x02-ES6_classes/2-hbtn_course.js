b89c7828a48346cf456d58bb8430de036db644f6
beckyshaz <sharonsakah8@gmail.com>
2024-10-03 13:16:51 +0000

    setter and getter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    }
+    if (
+      Array.isArray(students)
+      && students.every((student) => typeof student === 'string')) {
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newname) {
+    this._name = newname;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newlength) {
+    this._length = newlength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newstudents) {
+    this._students = newstudents;
+  }
+}
