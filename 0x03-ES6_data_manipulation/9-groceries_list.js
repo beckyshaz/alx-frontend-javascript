b9321a0c0a4ea1be83eec9c3fddc2c6f98983b45
beckyshaz <sharonsakah8@gmail.com>
2024-10-08 10:01:08 +0000

    map objects

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const gross = new Map();
+  gross.set('Apples', 10);
+  gross.set('Tomatoes', 10);
+  gross.set('Pasta', 1);
+  gross.set('Rice', 1);
+  gross.set('Banana', 5);
+  return gross;
+}
