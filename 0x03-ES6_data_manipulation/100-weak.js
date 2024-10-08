f9e8e78288105ce3c77aa9e7c583afbcc3d401ef
beckyshaz <sharonsakah8@gmail.com>
2024-10-08 12:16:57 +0000

    weakmap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  const count = weakMap.get(endpoint);
+  weakMap.set(endpoint, count + 1);
+  if (count + 1 >= 5) {
+    throw Error('Endpoint load is high');
+  }
+}
