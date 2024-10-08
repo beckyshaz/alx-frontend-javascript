c0f216bd16d37fc73c9f5fd7d861371be8893c49
beckyshaz <sharonsakah8@gmail.com>
2024-10-08 10:16:05 +0000

    array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   const array = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'San Columbia' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   return array;
